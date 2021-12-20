e4fc5e13398c877ccde7dc0bede75f4fc607d4df
Kimjunho <widn45@naver.com>
2021-12-21 01:03:05 +0900

    commit day6

--- chapter01/src/main/java/UserDao.java
+++ chapter01/src/main/java/UserDao.java
@@ -9,6 +9,8 @@ import java.sql.SQLException;
 
 
 public class UserDao {
+
+	//private static UserDao INSTANCE; //싱글톤
 	/*
 	private SimpleConnectionMaker simpleConnectionMaker; //초기 한번만 오브젝트를 만들어 저장
 
@@ -16,20 +18,37 @@ public class UserDao {
 		this.simpleConnectionMaker = new SimpleConnectionMaker();
 	}
 	*/
-	private ConnectionMaker connectionMaker;
+	private ConnectionMaker connectionMaker;// 초기에 설정하면 사용중에는 바뀌지 않는 읽기전용 인스턴스 변수
+	//자신이 사용하는 다른 싱글톤 빈을 저장하려는 용도라면 인스턴스 변수를 사용해도 좋다.
+	private Connection c; //매번 새로운 값으로 바뀌는 정보를 담은 인스턴스 변수, 심각한 문제 발생
+	private User user;
 
 	public UserDao(ConnectionMaker connectionMaker){		//UserDao를 생성하는 쪽에다가 ConnectionMaker 타입을 책임을 맡겼다
 		//connectionMaker = new DConnectionMaker(); // 생성자를 호출해서 오브젝트를 생성하는 코드가 남아있다.
 		//초기 한번은 어떤 클래스의 오브젝트를 사용할지를 결정하는 코드는 제거되지 않았다.
 		//UserDao가 어떤 ConnectionMaker 구현 클래스의 오브젝트를 이용하게 할지를 결정하는 코드가 남았다.
 		this.connectionMaker = connectionMaker;
-
 	}
+	/*
+	public static synchronized UserDao getInstance(){
+		//싱글톤 패턴을 단점
+		//1. 생성자가 private 로 제한되어 있으므로 싱글톤 클래스 자신만이 자기 오브젝트를 만들도록 제한되어 있다.
+		//2. 테스트가 힘들다.
+		//3. 서버환경에서 싱글톤이 하나만 만들어지는 것을 보장하지 못한다.
+		//4. 싱글톤의 사용은 전역상태를 만들수 있기 때문에 바람직하지 못하다.
+		//이러한 작업을 스프링 레지스터리를 활용하여 스프링 컨테이너가 모두 해주기 때문에 편안하게 스프링컨테이너에 오브젝트을 생명주기를 맡겨 생성하도록 한다.
+		
 
+		if(INSTANCE == null)
+			INSTANCE = new UserDao(???);
+		return INSTANCE;
+		
+	}
+	 */
 	public void add(User user) throws SQLException, ClassNotFoundException {
 		//Connection c = getConnection(); //메소드 분리 패턴
 		//Connection c = simpleConnectionMaker.makeNewConnection(); //UserDao 가 DB 커넥션을 가져오는 정보를 너무 많이 알고있다
-		Connection c = connectionMaker.makeNewConnection();
+		this.c = connectionMaker.makeNewConnection();
 		PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
@@ -41,21 +60,21 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws SQLException, ClassNotFoundException {
-		Connection c = connectionMaker.makeNewConnection();
+		this.c = connectionMaker.makeNewConnection();
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 		
 		ResultSet rs = ps.executeQuery();
 		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+		this.user = new User();
+		this.user.setId(rs.getString("id"));
+		this.user.setName(rs.getString("name"));
+		this.user.setPassword(rs.getString("password"));
 		
 		rs.close();
 		ps.close();
 		c.close();
-		return user;
+		return this.user;
 	}
 	
 
--- chapter01/src/main/java/UserDaoTest.java
+++ chapter01/src/main/java/UserDaoTest.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        /*
         //ConnectionMaker connectionMaker = new DConnectionMaker();  //생성하는 것은 클라이언트의 책임
         //UserDao dao = new DaoFactory().userDao();
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class); //어플리케이션 컨택스트
@@ -24,6 +25,29 @@ public class UserDaoTest {
         System.out.println(user2.getName());
         System.out.println(user2.getPassword());
         System.out.println(user2.getId() + " 조회 성공");
+        */
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao dao1 = daoFactory.userDao();
+        UserDao dao2 = daoFactory.userDao();
+        //동일한 객체인가? -> 결과는 다른 오브젝트를 반환한다.
+        System.out.println(dao1);
+        System.out.println(dao2);
 
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao3 = context.getBean("userDao",UserDao.class);
+        UserDao dao4 = context.getBean("userDao",UserDao.class);
+        //동일한 객체인가? -> 동일한 객체 , 하나의 인스턴스
+        System.out.println(dao3);
+        System.out.println(dao4);
+        //스프링은 기본적으로 별다른 설정을 하지 않을 경우 내부에서 생성하는 빈 오브젝트를 싱글톤으로 만든다.
+        //싱글톤 : 애플리케이션안에 제한된 수, 대개 1개의 오브젝트만 만들어서 사용
+        /**
+         * 자바의 싱글톤 구현
+         * 1. 클래스 밖에서는 오브젝트를 생성하지 못하도록 생성자를 private
+         * 2. 생성된 싱글톤 오브젝트를 저장할 수 있는 자신과 같은 타입의 스태틱 필드를 정의
+         * 3. 스태틱 팩토리 메소드인 getInstance()를 만들고 이 메소드가 최초 호출되는 시점에서 한번만 오브젝트가 만들어지도록 한다.
+         * 4. 오브젝트가 만들어 진뒤, getInstance() ㅔ소드를 통해 이미 만들어진 스태틱 필드에 저장된 오브젝트를 리턴한다.
+         */
     }
 }
