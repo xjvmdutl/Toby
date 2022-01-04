c1197b95b63f48ab41d888239215082a1c10d8d2
Kimjunho <widn45@naver.com>
2022-01-04 21:24:16 +0900

    toby chapter03 day2

--- chapter03/src/main/java/dao/UserDao.java
+++ chapter03/src/main/java/dao/UserDao.java
@@ -1,7 +1,7 @@
 package dao;
 
 import connectionMaker.ConnectionMaker;
-import strategy.DeleteAllStatement;
+import strategy.JdbcContext;
 import strategy.StatementStrategy;
 import entity.User;
 
@@ -18,6 +18,12 @@ public class UserDao {
 
 	private Connection c;
 
+	private JdbcContext jdbcContext;
+	/*
+	public void setJdbcContext(JdbcContext jdbcContext) {
+		this.jdbcContext = jdbcContext;
+	}
+	*/
 	public UserDao(ConnectionMaker connectionMaker) {
 		this.connectionMaker = connectionMaker;
 	}
@@ -30,19 +36,46 @@ public class UserDao {
 	}
 
 	public void setDataSource(DataSource dataSource) {
+
+		this.jdbcContext = new JdbcContext();
+		this.jdbcContext.setDataSource(dataSource); // UserDao가 컨테이너 역할을 한다.
 		this.dataSource = dataSource;
 	}
 
-	public void add(User user) throws SQLException {
-		c = dataSource.getConnection();
-		PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
+	public void add(final User user) throws SQLException { //User는 변경되지 않음으로 final
+		//해당 이름조차 없어도 되는 익명내부 클래스로 변경하자
+		/*
+		class AddStatement implements StatementStrategy{
+			//클래스를 내부 클래스로 로컬클래스 이다.
+			//User user; //로컬변수는 내부 변수에 직접 접근이 가능하다
 
-		ps.executeUpdate();
-		ps.close();
-		c.close();
+			//public AddStatement(User user) {
+			//	this.user = user;
+			//}
+
+			//단 내부 클래스에서 외부의 변수를 사용할 때는 외부 변수는 반드시 final로 표기해야한다.
+			@Override
+			public PreparedStatement makePrepareStatement(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
+				return ps;
+			}
+		}
+		 */
+		//jdbcContextWithStatementStrategy(
+		this.jdbcContext.workWithStatementStrategy(
+				new StatementStrategy() {
+					@Override
+					public PreparedStatement makePrepareStatement(Connection c) throws SQLException {
+						PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
+						ps.setString(1, user.getId());
+						ps.setString(2, user.getName());
+						ps.setString(3, user.getPassword());
+						return ps;
+					}
+		});
 	}
 
 	public User get(String id) throws SQLException {
@@ -74,45 +107,17 @@ public class UserDao {
 	 */
 	public void deleteAll() throws SQLException {
 		//해당 메소드가 클라이언트 코드가 되서 동작되어야 한다.
-		StatementStrategy st = new DeleteAllStatement();
-		jdbcContextWithStatementStrategy(st);
+		//StatementStrategy st = new DeleteAllStatement();
+		this.jdbcContext.workWithStatementStrategy(
+				new StatementStrategy() {
+			@Override
+			public PreparedStatement makePrepareStatement(Connection c) throws SQLException {
+				return c.prepareStatement("delete from users");
+			}
+		});
 	}
 
-	private void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-		/**
-		 * try/catch/finally 문법 사용
-		 */
-		Connection c = null;
-		PreparedStatement ps = null;
-		try {
-			c = dataSource.getConnection();
 
-			//ps = makeStatement(c);//해당 위치에서 에러 발생시 메소드 실행이 중지, //변하는 부분
-			//1.변하는 부분만 메소드로 빼기
-			//2.템플릿메소드 패턴의 적용 : 변하지 않는 부분은 슈퍼클래스에 두고 변하는 부분은 추상메소드로 정의해서 서브클래스에서 오버라이드 하여 새롭게 정의해 쓰도록 한다
-			//메소드를 만들때마다 서브 클래스를 만들어서 사용해야 한다
-			//3.전략패턴의 적용 : 오브젝트를 둘로 분리하고 클래스 레벨에서는 인터페이스를 통해서만 의존하도록 만드는 전략패턴
-			//StatementStrategy strategy = new DeleteAllStatement(); //Strategy 가 인터페이스 뿐만 아니라 특정 구현 클래스 DeleteAllStatement까지 알고 있는게 이상하다
-			//ps = strategy.makeStatement(c);
-			ps = stmt.makePrepareStatement(c);
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally { //무조건 반환이 되도록
-			if (ps != null) {//해당 리소스들이 제대로 반환되지 않을 수 있다.
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
-	}
 	/*
 	private PreparedStatement makeStatement(Connection c) throws SQLException {
 		//자주 변경되는 부분을 메소드로 바꿨지만 오히려 반대로 되었다.
