8dc6e4a36263513be71aad6a1a21b4064afef193
Kimjunho <widn45@naver.com>
2022-01-21 01:43:42 +0900

    day2 chapter6

--- chapter06/src/main/java/service/UserServiceTx.java
+++ chapter06/src/main/java/service/UserServiceTx.java
@@ -6,9 +6,21 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class UserServiceTx implements UserService{
-    //이렇게 분리하여 얻는 장점
-    //1. 비지니스 로직을 담당하고 있는 UserServiceImpl의 코드를 작성할 때는 트랜잭션과 같은 기술적인 내용에는 신경쓰지 않아도 된다.
-    //      -> 트랜잭션은 DI를 이용해 UserService와 같은 트랜잭션 기능을 가진 오브젝트가 먼저저 실행 되도록 만들기만 하면된다.
+    /**
+     * 이렇게 분리하여 얻는 장점
+     * 비지니스 로직을 담당하고 있는 UserServiceImpl의 코드를 작성할 때는 트랜잭션과 같은 기술적인 내용에는 신경쓰지 않아도 된다.
+     *      트랜잭션은 DI를 이용해 UserService와 같은 트랜잭션 기능을 가진 오브젝트가 먼저저 실행 되도록 만들기만 하면된다.
+     */
+    /**
+     * 고립되는 테스트 만들기
+     * 단위 테스트는 테스트의 범위가 좁으면 좁을수록 오류를 찾고 에러를 발견하기 쉽다.
+     * 따라서 의존되는 관계를 최대한 끊어야 한다.
+     * 테스트 스텁을 이용하여 의존관계에 아무런 동작을 하지 않게 할수 있지만 void형을 반환할 경우 해당 결과를 테스트 스텁에서 저장하고, 반환하는 역할을 해야한다.
+     *
+     */
+    //
+    //1.
+    //      ->
     UserService userService;
     PlatformTransactionManager transactionManager;
 
--- chapter06/src/main/java/test/UserServiceTest.java
+++ chapter06/src/main/java/test/UserServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
@@ -28,6 +29,7 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 import static service.UserServiceImpl.MIN_LOGCOUNT_FOR_SILVER;
 import static service.UserServiceImpl.MIN_RECOMMEND_FOR_GOLD;
 
@@ -69,6 +71,58 @@ public class UserServiceTest {
         }
     }
 
+    static class MockUserDao implements UserDao{
+
+        private List<User> users; //레벨 업그레이드 후보 // User오브젝트 목록
+        private List<User> updated = new ArrayList<>(); //업그레이드 대상 오브젝트를 저장해둘 목록
+
+        public MockUserDao(List<User> users) {
+            this.users = users;
+        }
+
+        public List<User> getUpdated() {
+            return updated;
+        }
+
+        @Override
+        public List<User> getAll() { //스텁 기능 제공
+            return this.users;
+        }
+
+        @Override
+        public void update(User user) {
+            updated.add(user);
+        }
+
+
+        /**
+         * 테스트에 사용되지 않는 메소드들
+         */
+
+        @Override
+        public void add(User user) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void deleteAll() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public User get(String id) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int getCount() {
+            throw new UnsupportedOperationException();
+        }
+
+
+    }
+    
+    
     @Autowired
     UserServiceImpl userServiceImpl;
 
@@ -104,26 +158,59 @@ public class UserServiceTest {
     @Test
     @DirtiesContext
     public void upgradeLevels() throws Exception{
-        userDao.deleteAll();
-        for(User user : users)
-            userDao.add(user);
+        /*
+        //직접 생성하므로서 컨테이너 설정에 관련된 RunWith와 같은 어노테이션을 지울수가 있다.
+        //사용자 DB를 모두 제거하고 모두 DB에 등록하는 번거로운 작업도 필요 없다
+        UserServiceImpl userServiceImpl = new UserServiceImpl(); //고립된 테스트 대상은 직접 생성하면 된다.
+        
+        MockUserDao mockUserDao = new MockUserDao(this.users);
+        userServiceImpl.setUserDao(mockUserDao);
 
-        MockMailSender mockMailSender = new MockMailSender();
+        MockMailSender mockMailSender = new MockMailSender();  //메일 발송 여부 확인을 위한 목 오브젝트
+        userServiceImpl.setMailSender(mockMailSender);
+
+        userServiceImpl.upgradeLevels(); //테스트 대상 실행
+
+        List<User> updated = mockUserDao.getUpdated();
+        assertThat(updated.size(), is(2));
+        checkUserAndLevel(updated.get(0), "joytouch", Level.SILVER);
+        checkUserAndLevel(updated.get(1), "madnite1", Level.GOLD);
+
+        List<String> request = mockMailSender.getRequests();      //목오브젝트를 통한 결과 확인
+        assertThat(request.size(), is(2));
+        assertThat(request.get(0), is(users.get(1).getEmail()));  
+        assertThat(request.get(1), is(users.get(3).getEmail()));
+         */
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+
+        UserDao mockUserDao = mock(UserDao.class); //다이나믹 목 생성, 리턴값설정, DI 까지 3줄이면 된다
+        when(mockUserDao.getAll()).thenReturn(this.users);
+        userServiceImpl.setUserDao(mockUserDao);
+
+        MailSender mockMailSender = mock(MailSender.class); //리턴값 없는 객체는 더욱 쉽게
         userServiceImpl.setMailSender(mockMailSender);
 
         userServiceImpl.upgradeLevels();
 
-        checkLevelUpgraded(users.get(0), false);
-        checkLevelUpgraded(users.get(1), true);
-        checkLevelUpgraded(users.get(2), false);
-        checkLevelUpgraded(users.get(3), true);
-        checkLevelUpgraded(users.get(4), false);
+        verify(mockUserDao, times(2)).update(any(User.class));
+        verify(mockUserDao, times(2)).update(any(User.class));
+        verify(mockUserDao).update(users.get(1));
+        assertThat(users.get(1).getLevel(), is(Level.SILVER));
+        verify(mockUserDao).update(users.get(3));
+        assertThat(users.get(3).getLevel(), is(Level.GOLD));
 
 
-        List<String> request = mockMailSender.getRequests();
-        assertThat(request.size(), is(2));
-        assertThat(request.get(0), is(users.get(1).getEmail()));  
-        assertThat(request.get(1), is(users.get(3).getEmail()));
+        ArgumentCaptor<SimpleMailMessage> mailMessageArgumentCaptor
+                = ArgumentCaptor.forClass(SimpleMailMessage.class); //파라미터를 정밀 검사하기위해 캡처도 가능
+        verify(mockMailSender, times(2)).send(mailMessageArgumentCaptor.capture());
+        List<SimpleMailMessage> mailMessages = mailMessageArgumentCaptor.getAllValues();
+        assertThat(mailMessages.get(0).getTo()[0], is(users.get(1).getEmail()));
+        assertThat(mailMessages.get(1).getTo()[0], is(users.get(3).getEmail()));
+    }
+
+    private void checkUserAndLevel(User updated, String expectId, Level expectLevel) {
+        assertThat(updated.getId(), is(expectId));
+        assertThat(updated.getLevel(), is(expectLevel));
     }
 
     @Test
